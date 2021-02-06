b98b3c0c576e03fd76eb488a4be49d8e1469ece1
D8GER <xyb5to0ZCY@gmail.com>
2021-02-07 01:06:16 +0800

    fix error

--- src/main/java/com/xyz/caofancpu/d8ger/action/LinkAction.java
+++ src/main/java/com/xyz/caofancpu/d8ger/action/LinkAction.java
@@ -1,5 +1,8 @@
 package com.xyz.caofancpu.d8ger.action;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -10,6 +13,7 @@ import com.intellij.openapi.project.Project;
 import com.xyz.caofancpu.d8ger.core.LinkUrlEnum;
 import com.xyz.caofancpu.d8ger.setting.window.LinkDialogWrapper;
 import com.xyz.caofancpu.d8ger.util.CollectionUtil;
+import com.xyz.caofancpu.d8ger.util.ConstantUtil;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -30,35 +34,44 @@ public class LinkAction extends AnAction {
         final Document currentDocument = currentEditor.getDocument();
 
         // Perform a refresh of the current file
-        WriteCommandAction.runWriteCommandAction(currentProject, () -> handleLink(currentProject, currentDocument));
-
-    }
-
-    private void handleLink(Project currentProject, Document currentDocument) {
         String sectionText = currentDocument.getText();
+        boolean showHelp = false;
         if (StringUtils.isBlank(sectionText)) {
-            currentDocument.setText(LinkUrlEnum.loadHelpInfo());
-            return;
+            showHelp = true;
         }
-        Pair<String, String> linkPair = null;
         if (sectionText.contains(LinkUrlEnum.QR_CODE.getTopic())) {
             String[] split = sectionText.split(LinkUrlEnum.QR_CODE.getTopic());
             if (split.length < 2) {
-                currentDocument.setText(LinkUrlEnum.loadHelpInfo());
-                return;
+                showHelp = true;
             }
+        }
+        if (showHelp) {
+            WriteCommandAction.runWriteCommandAction(currentProject, () -> currentDocument.setText(LinkUrlEnum.loadHelpInfo()));
+            return;
+        }
+        executeLinkQRCodeWindow(currentProject, currentDocument);
+    }
+
+    private void executeLinkQRCodeWindow(Project currentProject, Document currentDocument) {
+        String keyText = currentDocument.getText();
+        Pair<String, String> linkPair = null;
+        if (keyText.contains(LinkUrlEnum.QR_CODE.getTopic())) {
+            String[] split = keyText.split(LinkUrlEnum.QR_CODE.getTopic());
             linkPair = Pair.of(split[0], split[1]);
         } else {
-            LinkUrlEnum match = CollectionUtil.findAnyInArrays(LinkUrlEnum.values(), LinkUrlEnum::getTopic, sectionText);
+            LinkUrlEnum match = CollectionUtil.findAnyInArrays(LinkUrlEnum.values(), LinkUrlEnum::getTopic, keyText);
             if (Objects.nonNull(match) && !Objects.equals(match, LinkUrlEnum.QR_CODE)) {
                 linkPair = Pair.of(match.getTopic(), match.getUrl());
             }
         }
-        if (Objects.nonNull(linkPair)) {
-            LinkDialogWrapper linkDialogWrapper = new LinkDialogWrapper(currentProject, linkPair);
-            linkDialogWrapper.show();
+        if (Objects.isNull(linkPair)) {
+            Notifications.Bus.notify(
+                    new Notification(ConstantUtil.NOTIFICATION_GROUP_VIEW_ID, "Wow, you need new feature!", "keyword: [" + keyText + "] doesn't support for now", NotificationType.INFORMATION)
+            );
+            linkPair = Pair.of(LinkUrlEnum.HELP.getTopic(), LinkUrlEnum.HELP.getUrl());
         }
-
+        LinkDialogWrapper linkDialogWrapper = new LinkDialogWrapper(currentProject, linkPair);
+        linkDialogWrapper.show();
     }
 
 }
